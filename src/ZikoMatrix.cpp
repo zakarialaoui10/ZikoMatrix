62cdb36aacc6cc1e73f373e886c64166400c4cce
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-04-17 13:42:34 +0000

    Update ZikoMatrix.cpp

--- src/ZikoMatrix.cpp
+++ src/ZikoMatrix.cpp
@@ -1,72 +1,32 @@
-#include "ZikoMatrix.h"
-
-// Constructor that takes the number of rows and columns
-Matrix::Matrix(int numRows, int numCols) {
-  rows = numRows;
-  cols = numCols;
-  data = new float*[rows];
-  for (int i = 0; i < rows; i++) {
-    data[i] = new float[cols];
-    for (int j = 0; j < cols; j++) {
-      data[i][j] = 0;
+#include "Matrix.h"
+template<typename T>
+Matrix<T>::Matrix(int r, int c) {
+    rows = r;
+    cols = c;
+    data = new T*[rows];
+    for(int i = 0; i < rows; i++) {
+        data[i] = new T[cols];
+        for(int j = 0; j < cols; j++) {
+            data[i][j] = T();
+        }
     }
-  }
 }
-
-// Constructor that takes a 2D array
-Matrix::Matrix(float** array, int numRows, int numCols) {
-  rows = numRows;
-  cols = numCols;
-  data = new float*[rows];
-  for (int i = 0; i < rows; i++) {
-    data[i] = new float[cols];
-    for (int j = 0; j < cols; j++) {
-      data[i][j] = array[i][j];
+template<typename T>
+Matrix<T>::Matrix(T** arr, int r, int c) {
+    rows = r;
+    cols = c;
+    data = new T*[rows];
+    for(int i = 0; i < rows; i++) {
+        data[i] = new T[cols];
+        for(int j = 0; j < cols; j++) {
+            data[i][j] = arr[i][j];
+        }
     }
-  }
 }
-
-// Destructor
-Matrix::~Matrix() {
-  for (int i = 0; i < rows; i++) {
-    delete[] data[i];
-  }
-  delete[] data;
-}
-
-// Get the number of rows in the matrix
-int Matrix::getRows() const {
-  return rows;
-}
-
-// Get the number of columns in the matrix
-int Matrix::getCols() const {
-  return cols;
-}
-
-// Get the element at the specified row and column
-float Matrix::get(int row, int col) const {
-  return data[row][col];
-}
-
-// Set the element at the specified row and column
-void Matrix::set(int row, int col, float value) {
-  data[row][col] = value;
-}
-
-// Multiply this matrix by another matrix
-Matrix Matrix::multiply(const Matrix& other) const {
-  if (cols != other.rows) {
-    // Error: matrices cannot be multiplied
-    return Matrix(0, 0);
-  }
-  Matrix result(rows, other.cols);
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < other.cols; j++) {
-      float sum = 0;
-      for (int k = 0; k < cols; k++) {
-        sum += data[i][k] * other.data[k][j];
-      }
-      result.data[i][j] = sum;
+template<typename T>
+Matrix<T>::~Matrix() {
+    for(int i = 0; i < rows; i++) {
+        delete[] data[i];
     }
- 
+    delete[] data;
+}
