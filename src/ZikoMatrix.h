81eb980e6d2802394143341d73a2f52b17f4c9fa
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-04-17 14:05:47 +0000

    Update ZikoMatrix.h

--- src/ZikoMatrix.h
+++ src/ZikoMatrix.h
@@ -2,14 +2,33 @@
 #define MATRIX_H
 template<typename T>
 class Matrix {
-  private:
+public:
     int rows;
     int cols;
     T** data;
 
-  public:
-    Matrix(int r, int c);
-    Matrix(T** arr, int r, int c);
-    ~Matrix();
+public:
+    Matrix(int r, int c) {
+        rows = r;
+        cols = c;
+        data = new T*[rows];
+        for(int i = 0; i < rows; i++) {
+            data[i] = new T[cols];
+            for(int j = 0; j < cols; j++) {
+                data[i][j] = T();
+            }
+        }
+    }
+
+    // Getter for accessing individual elements
+    T& operator()(int i, int j) {
+        return data[i][j];
+    }
+    // Setter for modifying individual elements
+    void set(int i, int j, T value) {
+        data[i][j] = value;
+    }
+    // rest of the Matrix class implementation...
 };
+
 #endif
