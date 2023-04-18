20d481e29a1346106b03f96acee6c09af438afcb
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-04-18 17:25:57 +0000

    Update ZikoMatrix.h

--- src/ZikoMatrix.h
+++ src/ZikoMatrix.h
@@ -1,56 +1,63 @@
 #ifndef MATRIX_H
 #define MATRIX_H
+
 #if defined(ARDUINO)
 #include <Arduino.h>
 #else
 #include <iostream>
 #endif
 
-template <int rows, int cols>
+template <typename T, int rows, int cols>
 class Matrix {
 private:
-  int data[rows][cols];
+  T data[rows][cols];
 public:
-  Matrix(int (*arr)[cols]) {
+  Matrix(T (*arr)[cols]) {
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < cols; j++) {
         data[i][j] = arr[i][j];
       }
     }
   }
-  Matrix<rows, cols>(const int (&arr)[rows * cols]) {
-  for (size_t i = 0; i < rows; i++) {
-    for (size_t j = 0; j < cols; j++) {
-      data[i][j] = arr[i * cols + j];
+  
+  Matrix< T, rows, cols >(const T (&arr)[rows * cols]) {
+    for (size_t i = 0; i < rows; i++) {
+      for (size_t j = 0; j < cols; j++) {
+        data[i][j] = arr[i * cols + j];
+      }
     }
   }
-}
-  Matrix<rows, cols> clone() const {
-    Matrix<rows, cols> result = *this;
+
+  Matrix< T, rows, cols > clone() const {
+    Matrix< T, rows, cols > result = *this;
     return result;
   }
-  
-  int* operator[](int i) {
+
+  T* operator[](int i) {
     return data[i];
   }
-  int& operator()(int row, int col) {
+
+  T& operator()(int row, int col) {
     return data[row][col];
   }
 
-  const int& operator()(int row, int col) const {
+  const T& operator()(int row, int col) const {
     return data[row][col];
   }
-  Matrix<rows, cols> operator+(const Matrix<rows, cols>& other) const {
-      Matrix<rows, cols> result = this->clone();
-      result += other;
-      return result;
+
+  Matrix< T, rows, cols > operator+(const Matrix< T, rows, cols >& other) const {
+    Matrix< T, rows, cols > result = this->clone();
+    result += other;
+    return result;
   }
-  Matrix<rows, cols> operator-(const Matrix<rows, cols>& other) const {
-      Matrix<rows, cols> result = this->clone();
-      result -= other;
-      return result;
+
+  Matrix< T, rows, cols > operator-(const Matrix< T, rows, cols >& other) const {
+    Matrix< T, rows, cols > result = this->clone();
+    result -= other;
+    return result;
   }
-  Matrix<rows, cols>& operator+=(const Matrix<rows, cols>& other) {
+
+  Matrix< T, rows, cols >& operator+=(const Matrix< T, rows, cols >& other) {
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < cols; j++) {
         data[i][j] += other.data[i][j];
@@ -59,7 +66,7 @@ public:
     return *this;
   }
 
-  Matrix<rows, cols>& operator-=(const Matrix<rows, cols>& other) {
+  Matrix< T, rows, cols >& operator-=(const Matrix< T, rows, cols >& other) {
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < cols; j++) {
         data[i][j] -= other.data[i][j];
@@ -67,6 +74,7 @@ public:
     }
     return *this;
   }
+
   void print() const {
 #if defined(ARDUINO)
     for (int i = 0; i < rows; i++) {
@@ -86,4 +94,5 @@ public:
 #endif
   }
 };
+
 #endif // MATRIX_H
