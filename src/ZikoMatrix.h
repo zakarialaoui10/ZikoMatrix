82a1dec4a2b242316fead49d2f37688ce5eca573
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-04-20 22:51:19 +0000

    Add isDiag

--- src/ZikoMatrix.h
+++ src/ZikoMatrix.h
@@ -248,12 +248,20 @@ template<int new_cols>
       }
       return true;
   }
-  bool isId(){
+  bool isDiag(){
       if(!isSquare()||!isSym())return false;
       for(int i=0;i<_rows;i++){
           for(int j=0;j<_cols;j++){
-              if(i==j&&data[i][j]!=1)return false;
               if(i!=j&&data[i][j]!=0)return false;
+          }
+      }
+      return true;
+  }
+  bool isId(){
+      if(!isDiag())return false;
+      for(int i=0;i<_rows;i++){
+          for(int j=0;j<_cols;j++){
+              if(i==j&&data[i][j]!=1)return false;
           }
       }
       return true;
