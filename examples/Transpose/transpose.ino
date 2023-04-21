985218f45b1e98feaf195f9dd1f40cb2d08a06b8
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-04-21 15:04:50 +0000

    Update transpose.ino

--- examples/Transpose/transpose.ino
+++ examples/Transpose/transpose.ino
@@ -1,8 +1,10 @@
 #include "ZikoMatrix.h"
-int arr[2][4] = {{1,2,3,7}, {9,4,5,6}};
-Matrix<2, 4,int> m(arr);
 void setup() 
 {
   Serial.begin(9600);
-    m.print();
+  int arr[2][4] = {{1,2,3,7}, {9,4,5,6}};
+  Matrix<2, 4,int> m(arr);
+  m.print();
+  m.transpose();
+  m.print();
 }
