c34bd47d86f80cca299d35c00dbacfdf30dad105
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-04-22 20:31:40 +0000

    Update deleteCol.ino

--- examples/Transformation/DeleteCol/deleteCol.ino
+++ examples/Transformation/DeleteCol/deleteCol.ino
@@ -1,1 +1,11 @@
-
+#include "ZikoMatrix.h"
+double arr[4][3] = {{0,1,2}, {3,4,5},{6,7,8}, {9,10,11}};void setup() 
+{
+  Serial.begin(9600);  
+  Matrix<4, 3,double> m(arr);
+  m.print();
+  m.deleteCol(1);
+  m.print();
+  m.deleteCol(0);
+  m.print();
+}
