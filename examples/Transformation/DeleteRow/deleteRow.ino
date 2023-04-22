ba15b9353d77ecd75bcbb242e20c7b5bc7bdd43e
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-04-22 20:32:31 +0000

    Update deleteRow.ino

--- examples/Transformation/DeleteRow/deleteRow.ino
+++ examples/Transformation/DeleteRow/deleteRow.ino
@@ -1,1 +1,11 @@
-
+#include "ZikoMatrix.h"
+double arr[4][3] = {{0,1,2}, {3,4,5},{6,7,8}, {9,10,11}};
+void setup(){
+  Serial.begin(9600);  
+  Matrix<4, 3,double> m(arr);
+  m.print();
+  m.deleteRow(1);
+  m.print();
+  m.deleteRow(0);
+  m.print();
+}
