0ee861439870222e9ab6a447776cd530b040f6a4
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-04-22 18:14:49 +0000

    Update reshape.ino

--- examples/Transformation/Reshape/reshape.ino
+++ examples/Transformation/Reshape/reshape.ino
@@ -1,1 +1,14 @@
-
+#include "ZikoMatrix.h"
+int arr[2][3] = {{1,2,3}, {4,5,6}};
+Matrix<2, 3,int> m(arr);
+void setup() 
+{
+  Serial.begin(9600);
+    m.print();
+    m.reshape(3,2);
+    m.print();
+    m.reshape(6,1);
+    m.print();
+    m.reshape(1,6);
+    m.print();
+}
