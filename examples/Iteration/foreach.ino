e58cee7461c1911d9d83b0c16c20144f698362bf
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-04-22 18:39:24 +0000

    Update foreach.ino

--- examples/Iteration/foreach.ino
+++ examples/Iteration/foreach.ino
@@ -1,1 +1,11 @@
-
+#include "ZikoMatrix.h"
+int arr[2][3] = {{1,2,3}, {4,5,6}};
+Matrix<2, 3,int> m(arr);
+void setup() 
+{
+  Serial.begin(9600);
+  m.print();
+  auto multiply_by_two = [](int x) { return x * 2; };
+  m.foreach(multiply_by_two);
+  m.print();
+}
