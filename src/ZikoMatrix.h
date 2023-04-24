8a4e299c4d3c139189ef521b9d402afa764f3284
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-04-24 17:45:52 +0000

    Update ZikoMatrix.h

--- src/ZikoMatrix.h
+++ src/ZikoMatrix.h
@@ -9,28 +9,6 @@
 #else
 #include <iostream>
 #endif
-template<typename T>
-T _clamp(T x,T min,T max){
-   return x<min?min:x>max?max:x; 
-}
-template<typename T>
-T _lerp(T x,T min,T max){
-    return (max-min)*x+min;
-}
-template<typename T>
-T _norm(T x,T min,T max){
-    return (x-min)/(max-min);
-}
-template<typename T>
-T _map(T x,T a1,T b1,T a2,T b2){
-    return _lerp(_norm(x,a1,b1),a2,b2);
-}
-template<typename T>
-T _pow(T x,int n){
-    T p=1;
-    for(int i=0;i<n;i++)p*=x;
-    return p;
-}
 template <int rows = 0, int cols = rows , typename T=int>
 class Matrix {
     public:
