f2d5a814b2898cb5aaece3e2ec0cf82a8ca30232
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-04-25 16:50:00 +0000

    Delete examples directory

--- examples/Determinant/det.ino
+++
@@ -1,8 +0,0 @@
-#include "ZikoMatrix.h"
-double arr[3][3] = {{0,1,2},{3,4,5},{6,7,8}};
-void setup(){
-  Serial.begin(9600);  
-  Matrix<3, 3,double> M(arr);
-  Serial.print("The determinant of M is : ");
-  Serial.print(M.det());
-}
--- examples/Iteration/Lerp/lerp.ino
+++
@@ -1,9 +0,0 @@
-#include "ZikoMatrix.h"
-double arr[2][3] = {{1,2,3}, {4,5,6}};
-void setup() 
-{
-  Serial.begin(9600);
-  Matrix<2, 3,double> m(arr);
-  m.lerp(0,10);
-  m.print();
-}
